24722173a5b3d1e4c8c976640db621f2f1067450
Bradley <calvinandhobbs28@gmail.com>
2022-11-19 21:29:58 -0700

    new site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
-// @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-  buildOptions: {
-    site: 'https://astro-minimal-starter.netlify.app/',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true,      // Generate sitemap (set to "false" to disable)
-  },
-  // Comment out "renderers: []" to enable Astro's default component support.
-  renderers: ['@astrojs/renderer-preact'],
-});
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({});
